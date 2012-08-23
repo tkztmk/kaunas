d682f424da3598c7d1b06dd083476214847ea74f
tkztmk <tkztmk@outlook.com>
2012-08-24 05:26:51 +0900

    Add contact information to footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,12 @@ module ApplicationHelper
   def site_title
     Settings.site_title
   end
+  def site_contact
+    Settings.site_contact
+  end
+  def site_author
+    Settings.site_author
+  end
   def full_title(title)
     title ? "#{title} | #{site_title}" : site_title
   end
