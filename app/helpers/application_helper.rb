edcc86c2fe3b3acf5dde09451746f8e560c72abf
tkztmk <tkztmk@outlook.com>
2012-08-24 05:14:54 +0900

    Add configuration of website name to settings.yml.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
+  def site_title
+    Settings.site_title
+  end
   def full_title(title)
-    title ? "#{title} | kaunas" : 'kaunas'
+    title ? "#{title} | #{site_title}" : site_title
   end
   def root_path
     ENV['RAILS_ROOT_PATH'] || '/'
