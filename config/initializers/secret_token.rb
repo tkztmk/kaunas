988265f4fe11b05ed16a44596094acda5b516c6c
tkztmk <tkztmk@outlook.com>
2012-08-24 03:00:59 +0900

    Initialize kaunas with `rails new kaunas`, for initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaunas::Application.config.secret_token = 'ab3538f7a47d69fa04b9e29c31c885f1a39b56cbc15a756a1d9e1650603ca3c094abc695519d0aa46f7d39c9a214db1183a8c97a410d9fe245709b02091e6d91'
