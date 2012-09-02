9641d3c64b7073f9318423f606c3edf1077f9e70
tkztmk <tkztmk@outlook.com>
2012-09-02 09:47:35 +0900

    Add initial support for Google Custom Search.

---
+++ app/assets/javascripts/_google_custom_search.js
@@ -0,0 +1,7 @@
+(function() {
+  var cx = '001358686583477727398:fiw0b4scydm';
+  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + 
+    '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
