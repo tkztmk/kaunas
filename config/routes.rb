884d7b0eebebb79094f71a0940aa4a1d6749c8b5
tkztmk <tkztmk@outlook.com>
2012-09-02 10:30:22 +0900

    Introduce ActiveAdmin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Kaunas::Application.routes.draw do
 
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :articles 
 
   # The priority is based upon order of creation:
